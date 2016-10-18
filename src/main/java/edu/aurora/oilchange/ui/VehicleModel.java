9035c28566a692bdac21162c65560a8f8f11bb0d
Chris Stankus <cdstankus@gmail.com>
2016-10-18 10:45:58 -0500

    Add VehicleModel class

---
+++ src/main/java/edu/aurora/oilchange/ui/VehicleModel.java
@@ -0,0 +1,62 @@
+package edu.aurora.oilchange.ui;
+
+import edu.aurora.oilchange.Vehicle;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class VehicleModel {
+    private Vehicle vehicle;
+
+    private StringProperty make;
+    private StringProperty model;
+    private StringProperty year;
+
+    public VehicleModel() {
+        vehicle = new Vehicle();
+
+        make = new SimpleStringProperty(this, "make", "");
+        model = new SimpleStringProperty(this, "model", "");
+        year = new SimpleStringProperty(this, "year", "");
+
+        make.addListener((observable, oldValue, newValue) -> vehicle.setMake(newValue));
+        model.addListener((observable, oldValue, newValue) -> vehicle.setModel(newValue));
+        year.addListener((observable, oldValue, newValue) -> vehicle.setYear(newValue));
+    }
+
+    public String getMake() {
+        return make.get();
+    }
+
+    public StringProperty makeProperty() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make.set(make);
+    }
+
+    public String getModel() {
+        return model.get();
+    }
+
+    public StringProperty modelProperty() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model.set(model);
+    }
+
+    public String getYear() {
+        return year.get();
+    }
+
+    public StringProperty yearProperty() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year.set(year);
+    }
+}
