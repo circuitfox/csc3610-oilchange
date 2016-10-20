a165fe4b214eddbec3d47d21b6623c2f89d0d765
Chris Stankus <cdstankus@gmail.com>
2016-10-19 22:08:47 -0500

    Add oil change model class
    For displaying the total cost of labor.

--- src/main/java/edu/aurora/oilchange/OilChange.java
+++ src/main/java/edu/aurora/oilchange/OilChange.java
@@ -1,38 +1,40 @@
 package edu.aurora.oilchange;
 
-public class OilChange {
-	
-	private static final int PER_HOUR = 15;//&15 an hour
-	private static final double TAX = .07;//7%
-	private double laborHours;
-	
-	
-	
-	public OilChange() {
-		
-	}
-	
-	public OilChange(double laborHours) {
-		this.laborHours = laborHours;
-	}
-	
-	public static int getPerHour() {
-		return PER_HOUR;
-	}
+import java.math.BigDecimal;
 
-	public static double getTax() {
-		return TAX;
-	}
-	
-	public double getLaborHours() {
-		return this.laborHours;
-	}
-	
-	public double getTotal() {
-		return (this.laborHours*PER_HOUR)+((this.laborHours*PER_HOUR)*TAX);
-	}
-	
-	
-	
-	
+public class OilChange {
+    public static final int PER_HOUR = 15; // $15 an hour
+    public static final double TAX = .07; // 7%
+
+    private double laborHours;
+
+    public OilChange() {
+        this.laborHours = 0;
+    }
+
+    public OilChange(double laborHours) {
+        this.laborHours = laborHours;
+    }
+
+    public double getLaborHours() {
+        return this.laborHours;
+    }
+
+    public void setLaborHours(double laborHours) {
+        this.laborHours = laborHours;
+    }
+
+    public BigDecimal getTotal() {
+        return BigDecimal.valueOf(this.laborHours)
+                .multiply(BigDecimal.valueOf(PER_HOUR))
+                .multiply(BigDecimal.valueOf(1 + TAX));
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("OilChange{");
+        sb.append("laborHours=").append(laborHours);
+        sb.append('}');
+        return sb.toString();
+    }
 }
