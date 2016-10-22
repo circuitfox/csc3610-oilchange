501f76ddb63b7c8ed77203b0b156c6ab7dd3f578
Chris Stankus <cdstankus@gmail.com>
2016-10-22 13:06:11 -0500

    Validation utility class for controllers
    Also tests for same.

---
+++ src/main/java/edu/aurora/oilchange/controller/Validations.java
@@ -0,0 +1,76 @@
+package edu.aurora.oilchange.controller;
+
+/**
+ * Utility class for input validations
+ */
+public class Validations {
+    private Validations() {}
+
+    public static Validation digits(String input) {
+        return new Validation(input, "\\d");
+    }
+
+    public static Validation alphanumeric(String input) {
+        return new Validation(input, "\\w");
+    }
+
+    public static Validation alphabetical(String input) {
+        return new Validation(input, "[a-zA-z]");
+    }
+
+    /**
+     * Combinator for validations. Is given a pattern to match against,
+     * stores this pattern as a regex subgroup, and provides methods to expand
+     * this pattern into a boolean match.
+     */
+    static class Validation {
+        private String input;
+        private String pattern;
+
+        private boolean invert;
+
+        private Validation(String input, String pattern) {
+            this(input, pattern, false);
+        }
+
+        private Validation(String input, String pattern, boolean invert) {
+            this.input = input;
+            this.pattern = "(" + pattern + ")";
+            this.invert = invert;
+        }
+
+        public Validation not() {
+            return new Validation(input, pattern, true);
+        }
+
+        public boolean one() {
+            return input.matches(pattern) || invert;
+        }
+
+        public boolean any() {
+            pattern = pattern + "*";
+            return input.matches(pattern) || invert;
+        }
+
+        public boolean some() {
+            pattern = pattern + "+";
+            return input.matches(pattern) || invert;
+        }
+
+        public boolean repeat(int times) {
+            if (times < 1) {
+                return false;
+            }
+            pattern = pattern + "{" + times + "}";
+            return input.matches(pattern) || invert;
+        }
+
+        public boolean range(int from, int to) {
+            if (from < 1 || to < from) {
+                return false;
+            }
+            pattern = pattern + "{" + from + "," + to + "}";
+            return input.matches(pattern) || invert;
+        }
+    }
+}
---
+++ src/test/java/edu/aurora/oilchange/controller/ValidationsTest.java
@@ -0,0 +1,79 @@
+package edu.aurora.oilchange.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class ValidationsTest {
+    @Test
+    public void testOne() {
+        assertTrue("Could not match single digit", Validations.digits("0").one());
+
+        assertTrue("Could not match single alphanumeric character", Validations.alphanumeric("a").one());
+
+        assertTrue("Could not match single alphabetical character", Validations.alphabetical("A").one());
+
+        assertFalse("Matches multiple characters", Validations.digits("123").one());
+    }
+
+    @Test
+    public void testAny() {
+        assertTrue("Could not match zero digits with any()", Validations.digits("").any());
+        assertTrue("Could not match multiple digits with any()", Validations.digits("12345").any());
+
+        assertTrue("Could not match zero alphanumeric characters with any()",
+                Validations.alphanumeric("").any());
+        assertTrue("Could not match multiple alphanumeric with characters any()",
+                Validations.alphanumeric("abc123").any());
+
+        assertTrue("Could not match zero alphabetical characters with any()",
+                Validations.alphabetical("").any());
+        assertTrue("Could not match multiple alphabetical characters with any()",
+                Validations.alphabetical("abcdef").any());
+    }
+
+    @Test
+    public void testSome() {
+        assertTrue("Could not match one digit with some()", Validations.digits("1").some());
+        assertTrue("Could not match multiple digits with some()", Validations.digits("12345").some());
+
+        assertTrue("Could not match one alphanumeric character with some()",
+                Validations.alphanumeric("a").some());
+        assertTrue("Could not match multiple alphanumeric characters with some()",
+                Validations.alphanumeric("abc123").some());
+
+        assertTrue("Could not match one alphabetical character with some()",
+                Validations.alphabetical("a").some());
+        assertTrue("Could not match multiple alphabetical characters with some()",
+                Validations.alphabetical("abcdef").some());
+    }
+
+    @Test
+    public void testRepeat() {
+        assertFalse("Does not return false with times < 1", Validations.digits("123").repeat(-1));
+        assertEquals("repeat(1) is not equivalent to one()",
+                Validations.alphanumeric("a").repeat(1), Validations.alphanumeric("a").one());
+        assertTrue("Does not match correct number of repetitions", Validations.alphabetical("aaaa").repeat(4));
+        assertFalse("Matches with repetitions < input", Validations.digits("1234").repeat(3));
+    }
+
+    @Test
+    public void testRange() {
+        assertFalse("Does not return false with invalid range", Validations.digits("111").range(3, 2));
+        assertTrue("Does not match range with equal from and to", Validations.digits("111").range(3, 3));
+        assertTrue("Could not match upper bound of range", Validations.digits("123").range(2, 3));
+        assertTrue("Could not match lower bound of range", Validations.alphanumeric("a2").range(2, 3));
+        assertTrue("Could not match middle of range", Validations.alphabetical("abcde").range(3, 6));
+    }
+
+    @Test
+    public void testNot() {
+        assertTrue("Does not invert one()", Validations.digits("123").not().one());
+        assertTrue("Does not invert any()", Validations.alphanumeric("!@?").not().any());
+        assertTrue("Does not invert some()", Validations.alphabetical("1234").not().some());
+        assertTrue("Does not invert repeat()", Validations.digits("113").not().repeat(4));
+        assertTrue("Does not invert range()", Validations.alphabetical("1").not().range(2, 5));
+    }
+}
