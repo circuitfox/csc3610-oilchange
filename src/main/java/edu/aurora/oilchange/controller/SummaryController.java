772fe16b454d2cc55fcfa4d0e78f1792d95e4a73
Chris Stankus <cdstankus@gmail.com>
2016-10-23 09:47:40 -0500

    Update SummaryController to use models
    Also, give OilChange a working default

--- src/main/java/edu/aurora/oilchange/controller/SummaryController.java
+++ src/main/java/edu/aurora/oilchange/controller/SummaryController.java
@@ -2,13 +2,20 @@ package edu.aurora.oilchange.controller;
 
 import edu.aurora.oilchange.Customer;
 import edu.aurora.oilchange.Main;
-import edu.aurora.oilchange.ui.AppLauncher;
+import edu.aurora.oilchange.ui.*;
+
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.converter.BigDecimalStringConverter;
+import javafx.util.converter.NumberStringConverter;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 public class SummaryController {
-	@FXML
+    @FXML
 	private Button btnBack;
 	@FXML
 	private Label lblMake;
@@ -30,30 +37,54 @@ public class SummaryController {
 	private Label lblFilterCost;
 	@FXML
 	private Label lblOilAmount;
-	@FXML
-	private Button btnRefresh;
+    @FXML
+    private Label lblTotalCost;
 	@FXML
 	private Button btnSave;
 
+	private VehicleModel vehicleModel;
+    private OilModel oilModel;
+    private OilChangeModel oilChangeModel;
+    private DateModel dateModel;
+
 	@FXML
 	private void initialize() {
-		// TODO: use models
-		btnBack.setOnAction(e -> {
-			AppLauncher.root.setCenter(AppLauncher.oil);
-		});
-		btnRefresh.setOnAction(e -> {
-			lblMake.setText(Main.vehicle.getMake());
-			lblModel.setText(Main.vehicle.getModel());
-			lblYear.setText(Main.vehicle.getYear());
-			lblDate.setText(Main.vehicle.getDate().toString());
-			lblOilType.setText(Main.oil.getOilType());
-			lblOilBrand.setText(Main.oil.getOilBrand());
-			lblOilCost.setText(Main.oil.getPricePerQuart().toString());
-			lblOilAmount.setText(String.valueOf(Main.oil.getQuantity()));
-			lblFilterBrand.setText(Main.oil.getFilterBrand());
-			lblFilterCost.setText(Main.oil.getFilterCost().toString());
-		});
+        NumberStringConverter numberStringConverter = new NumberStringConverter();
+        BigDecimalStringConverter bigDecimalStringConverter = new BigDecimalStringConverter();
+        DecimalFormat currencyFormat = new DecimalFormat("#0.00");
+
+        lblMake.textProperty().bind(vehicleModel.makeProperty());
+        lblModel.textProperty().bind(vehicleModel.modelProperty());
+        lblYear.textProperty().bind(vehicleModel.yearProperty());
+
+        lblDate.textProperty().bind(Bindings.createStringBinding(dateModel::toString,
+                dateModel.monthProperty(), dateModel.dayProperty(), dateModel.yearProperty()));
+
+        lblOilType.textProperty().bind(oilModel.oilTypeProperty());
+        lblOilBrand.textProperty().bind(oilModel.oilBrandProperty());
+        lblOilAmount.textProperty().bind(Bindings.createStringBinding(() -> oilModel.getQuantity() + "qt",
+                oilModel.quantityProperty()));
+        lblOilCost.textProperty().bind(Bindings.createStringBinding(() ->
+                "$" + currencyFormat.format(oilModel.getPricePerQuart()) + "/qt",
+                oilModel.pricePerQuartProperty()));
+        lblFilterBrand.textProperty().bind(oilModel.filterBrandProperty());
+        lblFilterCost.textProperty().bind(Bindings.createStringBinding(
+                () -> "$" + currencyFormat.format(oilModel.getFilterCost()),
+                oilModel.filterCostProperty()));
+
+        lblTotalCost.textProperty().bind(Bindings.createStringBinding(() -> {
+            BigDecimal total = oilModel.getPricePerQuart()
+                    .multiply(BigDecimal.valueOf(oilModel.getQuantity()))
+                    .add(oilModel.getFilterCost()).add(oilChangeModel.getTotal());
+            return "$" + currencyFormat.format(total);
+        }, oilModel.quantityProperty(), oilModel.pricePerQuartProperty(),
+                oilModel.filterCostProperty(), oilChangeModel.laborHoursProperty()));
+
+		btnBack.setOnAction(e -> AppLauncher.root.setCenter(AppLauncher.oil));
+
+        // TODO: After summary, fire off DB command.
 		btnSave.setOnAction(e -> {
+            // FIXME: Replace this with setters to Main for now.
 			int id = (int) (Math.random() * 999999);
 			Main.customer = new Customer(Main.vehicle, Main.oil, id);
 			System.out.println("Information stored. Customer ID is " + id);
@@ -61,4 +92,19 @@ public class SummaryController {
 
 	}
 
+    public void setVehicleModel(VehicleModel vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public void setOilModel(OilModel oilModel) {
+        this.oilModel = oilModel;
+    }
+
+    public void setOilChangeModel(OilChangeModel oilChangeModel) {
+        this.oilChangeModel = oilChangeModel;
+    }
+
+    public void setDateModel(DateModel dateModel) {
+        this.dateModel = dateModel;
+    }
 }
