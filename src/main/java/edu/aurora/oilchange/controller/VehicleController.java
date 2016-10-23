c78d69319a01330c9f1b50c1c9be522c0d0f9fa7
Chris Stankus <cdstankus@gmail.com>
2016-10-22 20:31:40 -0500

    Use models in VehicleController
    Also give the DatePicker a default value; this lets us simplify validation a bit more.

--- src/main/java/edu/aurora/oilchange/controller/VehicleController.java
+++ src/main/java/edu/aurora/oilchange/controller/VehicleController.java
@@ -1,14 +1,16 @@
 package edu.aurora.oilchange.controller;
 
-import edu.aurora.oilchange.Main;
-import edu.aurora.oilchange.Vehicle;
 import edu.aurora.oilchange.ui.AppLauncher;
+import edu.aurora.oilchange.ui.DateModel;
+import edu.aurora.oilchange.ui.VehicleModel;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
 
 public class VehicleController {
 	@FXML
@@ -26,50 +28,56 @@ public class VehicleController {
 	@FXML
 	private TextArea txtWarning;
 
+	private VehicleModel vehicleModel;
+	private DateModel dateModel;
+
 	@FXML
 	private void initialize() {
-		// TODO: Use models, fix scaling, fix indentation.
+        dtDate.setValue(LocalDate.of(dateModel.getYear(), dateModel.getMonth(), dateModel.getDay()));
+
+		txtMake.textProperty().bind(vehicleModel.makeProperty());
+        txtModel.textProperty().bind(vehicleModel.modelProperty());
+		txtYear.textProperty().bind(vehicleModel.yearProperty());
+
+		dtDate.valueProperty().addListener((observable, oldValue, newValue) -> {
+			dateModel.setMonth(newValue.getDayOfMonth());
+			dateModel.setDay(newValue.getDayOfMonth());
+			dateModel.setYear(newValue.getYear());
+		});
+
 		btnNext.setOnAction(e -> {
-			Vehicle vehicle = new Vehicle();
             boolean fail = false;
             String error = "";
 
-            if (Validations.digits(txtMake.getText()).not().any()) {
-				vehicle.setMake((txtMake.getText()));
-            } else {
+            if (Validations.digits(txtMake.getText()).any()) {
                 error += "Please Only use letters for the make.";
                 fail = true;
 			}
 
-			if (Validations.digits(txtModel.getText()).not().any()) {
-				vehicle.setModel((txtModel.getText()));
-			} else {
+			if (Validations.digits(txtModel.getText()).any()) {
 				error += " Please Only use letters for the model.";
                 fail = true;
 			}
 
-			if (Validations.digits(txtYear.getText()).repeat(4)) {
-				vehicle.setYear((txtYear.getText()));
-			} else {
+			if (!Validations.digits(txtYear.getText()).repeat(4)) {
 				error += " Please only use four numbers for the year.";
-				fail = true;
-			}
-
-			if (dtDate.getValue() != null) {
-				vehicle.setDate(dtDate.getValue());
-			} else {
-				error += " Please enter a valid date.";
 				fail = true;
 			}
 
 			if (fail) {
 				txtWarning.setText(error);
 			} else {
-				Main.vehicle = vehicle;
-				System.out.println(Main.vehicle.getMake());
 				AppLauncher.root.setCenter(AppLauncher.oil);
 			}
 		});
 		btnCancel.setOnAction(e -> System.exit(1));
+	}
+
+	public void setVehicleModel(VehicleModel model) {
+		this.vehicleModel = model;
+	}
+
+	public void setDateModel(DateModel model) {
+		this.dateModel = model;
 	}
 }
